769f3df219c6957bf31cdcfe32d405d806130d98
yatume397techcamp <yatume397techcamp@gmail.com>
2023-10-09 00:27:37 +0900

    購入ボタンでフォームの値がテーブルに反映されることを確認

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :city
+    validates :addresses
+    validates :phone_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    # 注文情報を保存し、変数orderに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    # order_idには、変数orderのidと指定する
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
+
+
